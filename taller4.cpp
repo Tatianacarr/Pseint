4aaae0c1918bde42b3df61fd6465f685e308c036
Tatianacarr <anguifierro56@gmail.com>
2025-11-25 09:38:12 -0500

    Add files via upload

---
+++ taller4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+//Ejercicio 2
+int main(){
+	string nombres[3];
+	int edades[3];
+	for(int i=0;i<3;i++){
+		cout<<"Ingrese el nombre "<<i+1<<":";
+		cin>>nombres[i];
+		cout<<"Ingrese la edad "<<i+1<<":";
+		cin>>edades[i];
+	}
+	for(int i=0;i<3;i++){
+		cout<<nombres[i] <<" " <<edades[i]<<endl;
+		
+	}
+
+		
+	}
+	
